5522f55cb14d3882992acb587f85635643f946c3
LoTwT <709937065@qq.com>
2022-09-24 11:34:51 +0800

    feat(ts-compiler-api): add decorator

---
+++ packages/ts-compiler-api/addDecorator/addDecorator.ts
@@ -0,0 +1,37 @@
+import path from "node:path"
+import { Project, ClassDeclaration } from "ts-morph"
+
+const p = new Project()
+
+const source = p.addSourceFileAtPath(path.resolve(__dirname, "./source.ts"))
+
+const IMPLS = ["Handler"]
+
+// 获取所有目标 Class 声明
+const filteredClassDeclarations: ClassDeclaration[] = source
+  .getClasses()
+  .filter((cls) => {
+    const impls = cls.getImplements().map((impl) => impl.getText())
+    return IMPLS.some((impl) => impls.includes(impl))
+  })
+
+const METHODS = ["handle"]
+
+for (const cls of filteredClassDeclarations) {
+  // 拿到所有方法
+  const methods = cls.getMethods().map((method) => method.getName())
+
+  for (const method of methods) {
+    if (METHODS.includes(method)) {
+      // 拿到目标方法声明
+      const methodDeclaration = cls.getMethod(method)!
+
+      methodDeclaration.addDecorator({
+        name: "PerformanceMark",
+        arguments: [],
+      })
+    }
+  }
+}
+
+console.log(source.getText())
