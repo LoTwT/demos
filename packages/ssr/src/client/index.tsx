158e311fd2bad4fdaf0bfcff393951e9adcd167b
LoTwT <709937065@qq.com>
2022-10-21 13:39:07 +0800

    feat(ssr): render template page

---
+++ packages/ssr/src/client/index.tsx
@@ -0,0 +1,4 @@
+import { hydrateRoot } from "react-dom/client"
+import Home from "@/pages/Home"
+
+hydrateRoot(document.getElementById("root")!, <Home />)
--- packages/ssr/src/server/index.tsx
+++ packages/ssr/src/server/index.tsx
@@ -1,13 +1,20 @@
 import express from "express"
 import childProcess from "child_process"
+import { renderToString } from "react-dom/server"
+import Home from "@/pages/Home"
+import path from "node:path"
 
 const app = express()
+const content = renderToString(<Home />)
+
+app.use(express.static(path.resolve(process.cwd(), "client_build")))
 
 app.get("*", (req, res) => {
   res.send(`
 <html>
   <body>
-  <div>hello-ssr</div>
+    <div id="root">${content}</div>
+    <script src="/index.js"></script>
   </body>
 </html> 
 `)
