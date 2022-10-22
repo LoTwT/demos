01e6c7cef65db0ef81819eb94c95e42f3e58ee65
LoTwT <709937065@qq.com>
2022-10-22 21:49:58 +0800

    feat(official-site): customize head

--- packages/official-site/pages/_app.tsx
+++ packages/official-site/pages/_app.tsx
@@ -3,14 +3,26 @@ import type { AppProps, AppContext } from "next/app"
 import App from "next/app"
 import { Layout, ILayoutProps } from "@/components/layout/index"
 import code from "@/public/code.png"
+import Head from "next/head"
 
 function MyApp(props: AppProps & ILayoutProps) {
   const { Component, pageProps, navbarData, footerData } = props
 
   return (
-    <Layout navbarData={navbarData} footerData={footerData}>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>A Demo for 《深入浅出SSR官网开发指南》</title>
+        <meta
+          name="description"
+          content="A Demo for 《深入浅出SSR官网开发指南》"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Layout navbarData={navbarData} footerData={footerData}>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
