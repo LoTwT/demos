ea97c5020d81bef5140a08beaae9602cee5f795a
LoTwT <709937065@qq.com>
2022-10-22 21:25:46 +0800

    feat(official-site): data injection

--- packages/official-site/pages/_app.tsx
+++ packages/official-site/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css"
-import type { AppProps } from "next/app"
+import type { AppProps, AppContext } from "next/app"
+import App from "next/app"
 import { Layout, ILayoutProps } from "@/components/layout/index"
+import code from "@/public/code.png"
 
 function MyApp(props: AppProps & ILayoutProps) {
   const { Component, pageProps, navbarData, footerData } = props
@@ -10,6 +12,64 @@ function MyApp(props: AppProps & ILayoutProps) {
       <Component {...pageProps} />
     </Layout>
   )
+}
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  const pageProps = await App.getInitialProps(context)
+
+  return {
+    ...pageProps,
+    navbarData: {},
+    footerData: {
+      title: "Demo",
+      linkList: [
+        {
+          title: "技术栈",
+          list: [
+            {
+              label: "react",
+            },
+            {
+              label: "typescript",
+            },
+            {
+              label: "ssr",
+            },
+            {
+              label: "nodejs",
+            },
+          ],
+        },
+        {
+          title: "了解更多",
+          list: [
+            {
+              label: "掘金",
+              link: "https://juejin.cn/user/2714061017452557",
+            },
+            {
+              label: "知乎",
+              link: "https://www.zhihu.com/people/zmAboutFront",
+            },
+            {
+              label: "csdn",
+            },
+          ],
+        },
+        {
+          title: "联系我",
+          list: [{ label: "微信" }, { label: "QQ" }],
+        },
+      ],
+      qrCode: {
+        image: code,
+        text: "祯民讲前端微信公众号",
+      },
+      copyRight: "Copyright © 2022 xxx. 保留所有权利",
+      siteNumber: "粤ICP备XXXXXXXX号-X",
+      publicNumber: "粤公网安备 xxxxxxxxxxxxxx号",
+    },
+  }
 }
 
 export default MyApp
